c1cc98fb0845a5edf3a7c2b0769e12cb8d53929f
mj-xmr <mjxmr@protonmail.com>
2021-03-04 18:03:34 +0100

    Cbuild time analyser uses Unity

--- utils/health/clang-build-time-analyzer-run.sh
+++ utils/health/clang-build-time-analyzer-run.sh
@@ -58,6 +58,7 @@ cmake ../.. \
 -DCMAKE_C_COMPILER=clang \
 -DCMAKE_CXX_COMPILER=clang++ \
 -DUSE_CCACHE=OFF \
+-DUSE_UNITY=ON \
 -DUSE_COMPILATION_TIME_PROFILER=ON \
 -DBUILD_SHARED_LIBS=ON \
 -DBoost_INCLUDE_DIR="/home/enjo/devel/lib/tree/include" \
