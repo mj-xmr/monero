e0e32935ebf703d539aafcd49ef2cc6b03985ca4
mj-xmr <mjxmr@protonmail.com>
2021-03-05 18:47:13 +0100

    Xcompiling patch

--- contrib/depends/packages/libiconv.mk
+++ contrib/depends/packages/libiconv.mk
@@ -17,7 +17,8 @@ endef
 
 define $(package)_preprocess_cmds
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux/ &&\
-  patch -p1 < $($(package)_patch_dir)/fix-whitespace.patch
+  patch -p1 < $($(package)_patch_dir)/fix-whitespace.patch &&\
+  patch < $($(package)_patch_dir)/cross-compiling.patch
 endef
 
 define $(package)_config_cmds
